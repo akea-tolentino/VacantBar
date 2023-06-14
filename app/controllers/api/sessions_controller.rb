de4bc11d29986896c0f9de5f0d58fcd408c44e64
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-06-14 09:43:41 -0700

    completed backend auth

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Api::SessionsController < ApplicationController
+  before_action :require_logged_in, only: [:destroy]
+
+  def show
+    @user = current_user
+    if @user
+        render 'api/users/show'
+    else
+        render json: {user: nil}
+    end
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:credential], params[:password])
+    if @user
+        login!(@user)
+        render 'api/users/show'
+    else
+        render json: {errors: ['The provided credentials were invalid.'] }, status: :unauthorized
+    end
+  end
+
+  def destroy
+        logout!
+        render json: {message: ['success'] }
+  end
+end
---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,20 @@
+class Api::UsersController < ApplicationController
+    wrap_parameters include: User.attribute_names + ['password']
+
+    def create
+        @user = User.new(user_params)
+        @user.save
+        if @user
+            login!(@user)
+            render 'api/users/show'
+        else
+            render json: {errors: @user.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :fname, :lname, :username, :password)
+    end
+end
