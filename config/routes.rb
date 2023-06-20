f3291bd777d2f04e74a3b7a7765da722fd4eacce
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-06-20 10:15:20 -0700

    render deployment setup

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
     resource :session, only: [:show, :create, :destroy]
     resources :bars, only: [:index, :show]
   end
+  
+  get '$path', to: "static_pages#frontend_index"
 end
